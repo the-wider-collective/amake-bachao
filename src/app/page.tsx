13c0344a5f228caff805b63df018187cf1f9339f
Ehfaz Rezwan <ehfaz.rezwan@gmail.com>
2024-08-07 19:45:53 +0600

    Added some more copywriting and fixed styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/common/Hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 w-screen">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 w-screen">
       <div className="border-2 border-white w-[300px] p-6">
         <Hero />
       </div>
--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -10,9 +10,17 @@ const Hero = (props: Props) => {
       <h2 className="scroll-m-20 border-b pt-4 pb-4 text-3xl font-semibold tracking-tight first:mt-0 text-center">
         আমাকে বাচাও!
       </h2>
-      <p className="text-center">
+      <p className="pt-4 pb-4">
         Instant local help in emergencies - send alerts, get assistance, and
         build a safer community.
+      </p>
+      <p className="pt-4 pb-4">
+        A project to battle violence against minorities and innocent civilians
+        in Bangladesh. Once you sign up, you will be able to send and receive
+        emergency alerts to and from users in your area.
+      </p>
+      <p>
+        <a href="#">Read More</a>
       </p>
     </div>
   );
